794dcee383728dc5542c2f4867d8ad23568c3d3a
Mikhail Angelov <mikhail.angelov@gmail.com>
2016-07-29 10:12:51 +0300

    add detail view

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -1,19 +1,43 @@
 import React from 'react'
 import './details.css'
+import icon from '../../assets/placemark.png'
 
 export default ({details, onClose}) => {
 
 	if (details.marker) {
-		var descriptions = null
+		var description = null
+		var site = null
+		var region = null
+		var location = null
 		if (details.info.isFetching) {
-			descriptions = <div>spinner</div>
+			description = <div>todo spinner</div>
 		} else if (details.info.data) {
-			descriptions = <div className='description' dangerouslySetInnerHTML={{__html:details.info.data.description}}></div>
+			description = <div className='description' dangerouslySetInnerHTML={{__html:details.info.data.description}}></div>
+			if(details.info.data.site_url){
+				site = (<div className='subTitle'> <a href={details.info.data.site_url} target='blank'>{details.info.data.site_url}</a></div>)
+			}
+			if(details.info.data.area){
+				region = (<div className='subTitle'> {details.info.data.area.name}</div>)
+			}
+			if(details.marker.lat && details.marker.lng){
+				location = (<div className='subTitle' onClick={onClose}> <img src={icon}/></div>)
+			}
 		}
+		
+		
 		return (<div className='details'>
-			<img className='icon' src={ details.marker.logo['240']}/>
-			<div className='name'>{details.marker.name}</div>
-			{descriptions}
+			<div className='top'>
+				<div className='icon'>
+					<img src={ details.marker.logo['240']}/>
+				</div>
+				<div className='info'>
+					<div className='name'>{details.marker.name}</div>
+					{site}
+					{region}
+					{location}
+				</div>
+			</div>
+			{description}
 			<button className='details-close' onClick={onClose}></button>
 		</div>)
 	} else {
