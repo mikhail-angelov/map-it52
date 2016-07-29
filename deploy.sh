a3aa3923b5b56e0f2dc81a2746a34f2bfbddc685
Mikhail Angelov <mikhail.angelov@gmail.com>
2016-07-29 18:10:48 +0300

    add deployment script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+npm run build
+
+git checkout --orphan gh-pages
+
+rm -rf /tmp/deploy
+mkdir /tmp/deploy
+mv build /tmp/deploy/build
+mv node_modules /tmp/deploy/node_modules
+rm -rf *.*
+
+mv /tmp/deploy/build/*.* .
+git add .
+git commit -a -m "deployment"
+git push origin gh-pages
+
+rm -rf *.*
+mv /tmp/deploy/node_modules .
+rm -rf /tmp/deploy
+git checkout master
+git pull
