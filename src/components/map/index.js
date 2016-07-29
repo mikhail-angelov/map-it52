9385e16f8ef1a2ee872516f1f7c2d08b9cd1715a
Mikhail Angelov <mikhail.angelov@gmail.com>
2016-07-29 20:22:42 +0300

    add gmap key

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -12,18 +12,24 @@ export default class SimpleMapPage extends Component {
             lat={ marker.lat }
             lng={ marker.lng }
             key={ marker.id }
-            onClick={this.props.onShowBalloon.bind(this, marker)}
+            onClick={this.props.onShowBalloon.bind(this, marker) }
             />))
         const balloon = this.props.balloon ? (<Balloon marker={ this.props.balloon }
             lat={ this.props.balloon.lat }
             lng={ this.props.balloon.lng }
             key='balloon'
             onClose={this.props.onCloseBalloon }
-            onDetails={this.props.onShowDetails.bind(this,this.props.balloon) }/>) : null
+            onDetails={this.props.onShowDetails.bind(this, this.props.balloon) }/>) : null
         if (balloon) markers = _.concat([balloon], markers)
 
         return (<div style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100%' }}>
-            <GoogleMap defaultCenter={this.props.default.center} defaultZoom={this.props.default.zoom}>
+            <GoogleMap
+                defaultCenter={this.props.default.center}
+                defaultZoom={this.props.default.zoom}
+                bootstrapURLKeys={{
+                    key: AIzaSyBfYaSSHpk - h_m0DADxoXuf0fCYtsIcSA0,
+                    language: 'ru'
+                }}>
                 {markers}
             </GoogleMap>
         </div>)
