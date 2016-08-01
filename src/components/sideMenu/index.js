4bb7921e8cd84754f33633bff9cca81503c26216
Mikhail Angelov <mikhail.angelov@gmail.com>
2016-08-01 11:57:57 +0300

    add smove animation for side menu

--- src/components/sideMenu/index.js
+++ src/components/sideMenu/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Motion, spring} from 'react-motion';
 import './sideMenu.css'
 
 export default ({isOpen, results, onSearch, onToggle, onSelect}) => {
@@ -9,17 +10,31 @@ export default ({isOpen, results, onSearch, onToggle, onSelect}) => {
         stateClass = 'sideMenu open'
         inputText = <input className='term' onChange={event => onSearch(event.target.value) } />
         if (results) {
-            resultItems = results.map(item => (<div className='resultItem' onClick={()=>onSelect(item)}>{item.name}</div>))
+            resultItems = results.map(item => (<div
+                className='resultItem'
+                key={item.id}
+                onClick={() => onSelect(item) }>{item.name}</div>))
         }
     }
 
-    return (<div className={stateClass} >
-        <div className='top'>
-            <div className='searchIcon' onClick={onToggle}/>
-            {inputText}
-        </div>
-        <div className='results'>
-            {resultItems}
-        </div>
-    </div>)
+    return (
+        <Motion
+            defaultStyle={{width:50, height:50}}
+            style={{
+                width: isOpen ? spring(300) : spring(50),
+                height: isOpen ? spring(400) : spring(50)
+            }}>
+            {(style) => (<div className={stateClass} style={{width:style.width}} >
+                <div className='searchIcon' onClick={onToggle}></div>
+                <div className='top' style={{maxHeight:style.height}}>
+
+                    {inputText}
+                    <div className='results'>
+                        {resultItems}
+                    </div>
+                </div>
+
+            </div>)
+            }
+        </Motion>)
 }
